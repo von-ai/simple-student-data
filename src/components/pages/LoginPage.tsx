b4c1718bc94a81e2e778800dfae34fb02548d45b
von-ai <kuhhakuzuhdi@gmail.com>
2025-10-08 15:04:20 +0800

    feat: Update LoginPage to redirect to dashboard and enhance form layout

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -21,49 +21,54 @@ const LoginPage = () => {
 
   const onSubmit: SubmitHandler<LoginForm> = (data) => {
     if (data.username === admin && data.password === password) {
-      router.push('/');
+      router.push('/dashboard');
     }
   };
 
   return (
-    <section className="w-full h-auto bg-white p-6 max-w-md mx-auto mt-20 rounded-md shadow-md">
-      <h1 className="text-2xl font-semibold mb-6 text-center">Masuk</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <label className="mb-1 font-medium">Username</label>
-          <input
-            type="text"
-            {...register('username', { required: 'Username wajib diisi' })}
-            className="border border-gray-300 rounded-md px-3 py-2"
-          />
-          {errors.username && (
-            <span className="text-red-500 text-sm">
-              {errors.username.message}
-            </span>
-          )}
-        </div>
+    <section className="w-full h-screen flex justify-center items-center">
+      <section className="w-full h-auto bg-white p-6 max-w-md mx-auto mt-20 rounded-md shadow-md">
+        <h1 className="text-2xl font-semibold mb-6 text-center">Masuk</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <label className="mb-1 font-medium">Username</label>
+            <input
+              type="text"
+              {...register('username', { required: 'Username wajib diisi' })}
+              className="border border-gray-300 rounded-md px-3 py-2"
+            />
+            {errors.username && (
+              <span className="text-red-500 text-sm">
+                {errors.username.message}
+              </span>
+            )}
+          </div>
 
-        <div className="flex flex-col">
-          <label className="mb-1 font-medium">Password</label>
-          <input
-            type="password"
-            {...register('password', { required: 'Password wajib diisi' })}
-            className="border border-gray-300 rounded-md px-3 py-2"
-          />
-          {errors.password && (
-            <span className="text-red-500 text-sm">
-              {errors.password.message}
-            </span>
-          )}
-        </div>
+          <div className="flex flex-col">
+            <label className="mb-1 font-medium">Password</label>
+            <input
+              type="password"
+              {...register('password', { required: 'Password wajib diisi' })}
+              className="border border-gray-300 rounded-md px-3 py-2"
+            />
+            {errors.password && (
+              <span className="text-red-500 text-sm">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
 
-        <button
-          type="submit"
-          className="bg-primer text-white px-4 py-2 rounded-md hover:bg-hover transition"
-        >
-          Masuk
-        </button>
-      </form>
+          <button
+            type="submit"
+            className="bg-primer text-white px-4 py-2 rounded-md hover:bg-hover transition"
+          >
+            Masuk
+          </button>
+          <p className="text-gray-400 text-sm text-center">
+            Username: admin ; Password: 12345
+          </p>
+        </form>
+      </section>
     </section>
   );
 };
